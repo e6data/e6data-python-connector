fa048117d8175a5cae6ad1b824e47fa57350c586
Vishal Anand <101251245+vishale6x@users.noreply.github.com>
2023-06-06 18:13:45 +0530

    <Vishal> fetchall buffer added.

--- e6xdb/e6x.py
+++ e6xdb/e6x.py
@@ -425,7 +425,9 @@ class Cursor(DBAPICursor):
         self._data = None
         return rows
 
-    def fetchall_buffer(self):
+    def fetchall_buffer(self, query_id=None):
+        if query_id:
+            self._query_id = query_id
         while True:
             rows = self.fetch_batch()
             if not rows:
@@ -446,11 +448,15 @@ class Cursor(DBAPICursor):
         # one batch retrieves the predefined set of rows
         return read_rows_from_batch(self._query_columns_description, dis)
 
-    def fetchall(self):
+    def fetchall(self, query_id=None):
+        if query_id:
+            self._query_id = query_id
         return self._fetch_all()
 
-    def fetchmany(self, size=None):
+    def fetchmany(self, size=None, query_id=None):
         # _logger.info("fetching all from overriden method")
+        if query_id:
+            self._query_id = query_id
         if size is None:
             size = self.arraysize
         if self._data is None:
